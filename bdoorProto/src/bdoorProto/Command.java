bbdb0e0ef18be8ee1512acd521e26777feb9a35b
Yury Benesh <ybxsoft@tut.by>
2014-10-11 20:23:45 +0300

    proto lib

---
+++ bdoorProto/src/bdoorProto/Command.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bdoorProto;
+
+/**
+ *
+ * @author yur
+ */
+public class Command {
+	String cmd;
+	int argDataSize;
+	byte [] argData;
+	
+}
